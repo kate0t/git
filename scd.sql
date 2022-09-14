db0075f41eb1527a2eb4c085e64446cf8a9de839
kate0t <112848900+kate0t@users.noreply.github.com>
2022-09-14 12:00:00 +0300

    Update scd.sql

--- scd.sql
+++ scd.sql
@@ -15,10 +15,14 @@ DISTRIBUTED BY (user_id);
 
 --Вставка данных
 INSERT INTO student43.user_dim (user_key,user_id,first_name,last_name,address,zipcode,date_from,date_to,created_date,updated_date) VALUES
-	 (1000,'b0cc9fde-a29a-498e-824f-e52399991beb','john','doe','world','10027','1990-01-15','1990-03-01','1900-01-15 00:00:00.000','1900-01-15 10:00:00.000'),
-	 (1100,'b0cc9fde-a29a-498e-824f-e52399991beb','john','doe','world','1002777','1990-01-15','1990-03-01','1900-01-14 00:00:00.000','1900-01-15 00:00:00.000'),
-	 (1200,'b0cc9fde-a29a-498e-824f-e52399991beb','john','doe','world','10012','1990-01-01','1990-02-01','1900-01-01 00:00:00.000','1900-01-01 00:00:00.000');
-
+	 (1000,'b0cc9fde-a29a-498e-824f-e52399991beb','john','doe','world','10027','1990-01-15','1990-03-01','1900-01-15','1900-01-15 10:00:00.000'),
+	 (1100,'b0cc9fde-a29a-498e-824f-e52399991beb','john','doe','world','1002777','1990-01-15','1990-03-01','1900-01-14','1900-01-15 00:00:00.000'),
+	 (1200,'b0cc9fde-a29a-498e-824f-e52399991beb','john','doe','world','10012','1990-01-01','1990-02-01','1900-01-01','1900-01-01 00:00:00.000'),
+	 (1300,'x4d93qe-v12z-745b-098y-w12387652gun','clara','fluff','world','111111','1990-01-01','1990-02-01','1900-01-01','1900-01-01 00:00:00.000'),
+	 (1400,'x4d93qe-v12z-745b-098y-w12387652gun','clara','fluff','world','222222','1990-01-15','1990-02-01','1900-01-15','1900-01-15 00:00:00.000'),
+	 (1500,'x4d93qe-v12z-745b-098y-w12387652gun','clara','fluff','world','333333','1990-02-01','1990-03-01','1900-02-01','1900-02-01 00:00:00.000');
+	 
+	 
 --Создание витрины	
 	
 CREATE TABLE student43.user_dm (
@@ -43,7 +47,7 @@ from user_dim) user_dim
 where rank = 1;
 
 
---+первая и последняя запись
+--+первая и последняя запись, даты с и по не изменяются
 with rank_tmp as (
 select 
 	user_id,
@@ -55,7 +59,8 @@ select
 	date_to,
 	rank () over(partition by user_id order by date_to asc, updated_date asc
 	)
-from user_dim)
+from user_dim
+)
 , new_dates as (
 select user_id,
 	first_name,
@@ -84,7 +89,6 @@ select
 	date_to,
 	now() as upd_dttm
 from rank_tmp
-where rank not in ((select max(rank) from rank_tmp), 1)
 union 
 select 
 	user_id,
@@ -99,7 +103,58 @@ from new_dates
 where rank in ((select max(rank) from rank_tmp), 1)
 order by user_id, date_from
 
+--+первая и последняя запись, даты с и по изменяются
+with rank_tmp as (
+select 
+	user_id,
+	first_name,
+	last_name,
+	address,
+	zipcode,
+	date_from,
+	date_to,
+	rank () over(partition by user_id order by date_to asc, updated_date asc),
+	lead(date_from) over (order by date_from) -1 as prev_date
+from user_dim
+)
+, new_dates as (
+select user_id,
+	first_name,
+	last_name,
+	address,
+	zipcode,
+	date_from,
+	date_to,
+	rank,
+	case 
+		when rank = 1 then '1900-01-01' 
+		when rank = (select max(rank) from rank_tmp) then date_to +1
+	end as date_from_new,
+	case 
+		when rank = 1 then date_from - 1
+		when rank = (select max(rank) from rank_tmp) then '9999-12-31' 
+	end as date_to_new
+from rank_tmp)
+select 
+	user_id,
+	first_name,
+	last_name,
+	address,
+	zipcode,
+	date_from,
+	case when prev_date is null then date_to else prev_date end as date_to
+	from rank_tmp
+union 
+select 
+	user_id,
+	first_name,
+	last_name,
+	address,
+	zipcode,
+	date_from_new as date_from,
+	date_to_new as date_to
+from new_dates
+where rank in ((select max(rank) from rank_tmp), 1)
+order by user_id, date_from 
 
 
-Вопросы:
-1) Выравнивание истории - должны изменяться date_from/date_to с ранком между первым и последним?
